f4785036e74bbbe05a9dd2d56ee13b738f3bde1f
Phạm Văn Hiếu <102954024+hieuluxyry@users.noreply.github.com>
2024-06-24 21:29:10 +0700

    Delete SaleManager directory

--- SaleManager/CSVDataAccessObjectFactory.java
+++
@@ -1,9 +0,0 @@
-// Triển khai của DataAccessObjectFactory để tạo ra CSVProductDAO.
-public class CSVDataAccessObjectFactory implements DataAccessObjectFactory {
-
-    @Override
-    public ProductDAO getProductDAO() {
-        return new CSVProductDAO();
-    }
-}
-
--- SaleManager/DataAccessObjectFactory.java
+++
@@ -1,4 +0,0 @@
-//Interface của Abstract Factory để tạo ra các DAO
-public interface DataAccessObjectFactory {
-    ProductDAO getProductDAO();
-}
